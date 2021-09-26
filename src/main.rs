f2289f1a52035a542028810176f48fb397947225
Stef <stef.robbe@gmail.com>
2021-09-26 22:04:29 +0200

    set up with ctrl alt f4

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 use winapi;
 use winapi::shared::windef::HHOOK;
 use winapi::um::winuser;
-use winapi::um::winuser::{HC_ACTION, KBDLLHOOKSTRUCT, WH_KEYBOARD_LL, WM_KEYDOWN, WM_KEYUP};
+use winapi::um::winuser::{HC_ACTION, KBDLLHOOKSTRUCT, CallNextHookEx, WH_KEYBOARD_LL, WM_KEYDOWN, WM_SYSKEYDOWN, WM_SYSKEYUP, WM_KEYUP, VK_F4, VK_LMENU, VK_LCONTROL};
+use std::convert::TryFrom;
 
 static mut HOOK_HANDLE: Option<HHOOK> = None;
-static mut SHIFT: bool = false;
-
-const LSHIFT: u32 = 160;
-const RSHIFT: u32 = 161;
+static mut CTRL_DOWN: bool = false;
+static mut ALT_DOWN: bool = false;
+static mut F4_DOWN: bool = false;
 
 fn main() {
     unsafe {
@@ -31,10 +31,14 @@ fn main() {
 
 // https://docs.microsoft.com/en-us/previous-versions/windows/desktop/legacy/ms644985(v=vs.85)
 extern "system" fn hook_callback(code: i32, wparam: usize, lparam: isize) -> isize {
+    let CTRL: u32 = u32::try_from(VK_LCONTROL).unwrap();
+    let ALT: u32 = u32::try_from(VK_LMENU).unwrap();
+    let F4: u32 = u32::try_from(VK_F4).unwrap();
+
     if code < HC_ACTION {
         unsafe {
             if let Some(hook_id) = HOOK_HANDLE {
-                return winuser::CallNextHookEx(hook_id, code, wparam, lparam);
+                return CallNextHookEx(hook_id, code, wparam, lparam);
             } else {
                 return 0;
             }
@@ -42,54 +46,61 @@ extern "system" fn hook_callback(code: i32, wparam: usize, lparam: isize) -> isi
     }
 
     let keypress: KBDLLHOOKSTRUCT = unsafe { *(lparam as *mut KBDLLHOOKSTRUCT) };
-    let mut is_shift = keypress.vkCode == LSHIFT || keypress.vkCode == RSHIFT;
+    let is_ctrl = keypress.vkCode == CTRL;
+    let is_alt = keypress.vkCode == ALT;
+    let is_f4 = keypress.vkCode == F4;
 
-    if wparam == WM_KEYDOWN as usize {
+    if wparam == WM_KEYDOWN as usize || wparam == WM_SYSKEYDOWN as usize {
         unsafe {
-            if is_shift {
-                SHIFT = true;
-            } else {
-                let character: String = from_virtual_key_code(keypress.vkCode, SHIFT);
-                println!("{}", character);
+            if is_ctrl {
+                CTRL_DOWN = true;
+            } else if is_alt {
+                ALT_DOWN = true;
+            } else if is_f4 {
+                F4_DOWN = true;
             }
         }
-    } else if wparam == WM_KEYUP as usize {
+    }
+
+    unsafe {
+        if CTRL_DOWN && ALT_DOWN && F4_DOWN {
+            println!("Time to kill!");
+            // get foreground window
+            // kill that id
+            
+
+            // prevent this keypress from being propagated
+            return 1;
+        }
+    }
+    
+    // this can be neater but cba
+    if wparam == WM_KEYUP as usize {
         unsafe {
-            if is_shift {
-                SHIFT = false
+            if is_ctrl {
+                CTRL_DOWN = false
+            }
+            if is_alt {
+                ALT_DOWN = false
+            }
+            if is_f4 {
+                F4_DOWN = false
+            }
+        }
+    }
+    if wparam == WM_SYSKEYUP as usize {
+        unsafe {
+            if is_ctrl {
+                CTRL_DOWN = false
+            }
+            if is_alt {
+                ALT_DOWN = false
+            }
+            if is_f4 {
+                F4_DOWN = false
             }
         }
     }
 
     0
-}
-
-fn from_virtual_key_code(code: u32, shift: bool) -> String {
-    // TODO: See if we can leverage MapVirtualKeyA here?
-    // now we're assuming nordic QWERTY layout
-    match code {
-        65..=90 | 48..=57 => {
-            let string: String = (code as u8 as char).into();
-            match shift {
-                true => string,
-                false => string.to_lowercase(),
-            }
-        }
-        32 => "[space]".into(),
-        8 => "[backspace]".into(),
-        27 => "[esc]".into(),
-        112..=123 => format!("f{}", code - 111),
-        code if code == 188 && shift => ";".into(),
-        code if code == 188 && !shift => ",".into(),
-
-        code if code == 190 && shift => ":".into(),
-        code if code == 190 && !shift => ".".into(),
-
-        code if code == 191 && shift => "*".into(),
-        code if code == 191 && !shift => "'".into(),
-
-        code if code == 189 && shift => "_".into(),
-        code if code == 189 && !shift => "-".into(),
-        _ => format!("unknown ({})", code),
-    }
 }
